aafb9c860cad10ad3e40a54334a8e293154cac46
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-10-15 17:31:18 +0530

    Admin Setup Basic page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AdminLogin from "./admin/Login/Login.jsx"
 import Dashboard from "./admin/Dashboard/Index.jsx"
 import PaymentList from "./admin/PaymentList/PaymentList.jsx"
 import UserList from "./admin/UserList/userList.jsx"
-import BookingList from "./admin/Bookingmanagement/BookingList.jsx"
+import NotFoundPage from './admin/compontents/NotFoundPage.js';
 
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
     <>
       <Router>
         <Routes>
+        <Route path="*" element={<NotFoundPage />} /> 
           <Route path="/access-admin" element={<AdminLayout>   <Dashboard />          </AdminLayout>} />
           <Route path='/access-admin/login' element={<AdminLogin />} />
           <Route path="/access-admin/payment-list" element={<AdminLayout> <PaymentList /> </AdminLayout>} />
