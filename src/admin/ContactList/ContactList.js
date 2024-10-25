d627b7a4527c3034cd0da0b272259b72e57ad433
Abhinav-FP <a.mathur@futureprofilez.com>
2024-10-25 16:09:46 +0530

    Contact Listing added

--- src/admin/ContactList/ContactList.js
+++ src/admin/ContactList/ContactList.js
@@ -100,7 +100,6 @@ export default function ContactList() {
                       "Not replied yet"
                       }
                       </td>
-
                     </tr>
                   ))
                 )}
