6c6ce87f10f0ad358606d1668e41f212915dd278
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2024-12-21 16:31:49 +0530

    review , refral ,  instrutor

--- src/admin/Bookingmanagement/PaymentButton.jsx
+++ src/admin/Bookingmanagement/PaymentButton.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 const PaymentButton = ({ item, handlepayment, payment }) => {
+  console.log("payment", payment)
   const [canGeneratePayment, setCanGeneratePayment] = useState(false);
 
   useEffect(() => {
@@ -17,13 +18,16 @@ const PaymentButton = ({ item, handlepayment, payment }) => {
 
   return (
     <div>
-      {canGeneratePayment && (
-        <button
-          onClick={() => handlepayment(item?._id)}
-          className="bg-[#ff0062] hover:bg-[#4400c3] text-white font-bold text-[12px] md:text-[14px] py-[13px] px-[10px] md:px-[10px] rounded"
-        >
-          ReGenerate payment Link
-        </button>
+      {payment?.payment_status != "success" && (
+
+        canGeneratePayment && (
+          <button
+            onClick={() => handlepayment(item?._id)}
+            className="bg-[#ff0062] hover:bg-[#4400c3] text-white font-bold text-[12px] md:text-[14px] py-[13px] px-[10px] md:px-[10px] rounded"
+          >
+            ReGenerate payment Link
+          </button>
+        )
       )}
     </div>
   );
